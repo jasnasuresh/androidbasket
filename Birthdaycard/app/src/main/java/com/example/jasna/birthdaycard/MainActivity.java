de4fccf725009417732b97b793548a7ea0266acb
jasnasuresh <jasna.suresh@gmail.com>
2019-01-08 14:28:18 +0530

    birthday card app

---
+++ Birthdaycard/app/src/main/java/com/example/jasna/birthdaycard/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jasna.birthdaycard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+This repository consists of my works on android studio :)
